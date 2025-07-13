4d2f430cd3d292832e3128dbed9a1be4df914b2f
zj47i <zj47i@naver.com>
2025-07-13 17:50:25 +0900

    editor class

--- src/editor.ts
+++ src/editor.ts
@@ -1,17 +1,48 @@
-import { VDomNode } from "./vdom/vdom-node";
-import { VDomNodeType } from "./vdom/vdom-node.enum";
-import { Synchronizer } from "./syncronizer/syncronizer";
 import { DomNode } from "./dom/dom-node";
+import { VDomNode } from "./vdom/vdom-node";
+import { Synchronizer } from "./syncronizer/syncronizer";
+import { CompositionStateMachine } from "./state-machine/composition.state-machine";
+import { SelectionStateMachine } from "./state-machine/selection.state-machine";
+import { Command } from "./command/command";
+import { BackspaceHandler } from "./command/backspace.handler";
+import { EnterHandler } from "./command/enter.handler";
 
 export class Editor {
-    constructor(
-        private dom: DomNode,
-        private vdom: VDomNode,
-        private sync: Synchronizer
-    ) {
-        this.sync.appendNewVDomNodeWithoutHook(
+    public dom: DomNode;
+    public vdom: VDomNode;
+    public sync: Synchronizer;
+    public selectionStateMachine: SelectionStateMachine;
+    public compositionStateMachine: CompositionStateMachine;
+    public backspaceHandler: BackspaceHandler;
+    public enterHandler: EnterHandler;
+    public command: Command;
+
+    constructor(editorId: string) {
+        const editorDiv = document.getElementById(editorId);
+        if (!(editorDiv instanceof HTMLDivElement)) {
+            throw new Error("editor element not found");
+        }
+        editorDiv.setAttribute("contenteditable", "true");
+
+        this.dom = new DomNode(editorDiv);
+        this.vdom = VDomNode.createRootNode();
+        this.selectionStateMachine = new SelectionStateMachine(editorDiv);
+        this.compositionStateMachine = new CompositionStateMachine(editorDiv);
+        this.sync = new Synchronizer(
+            this.dom,
             this.vdom,
-            new VDomNode(VDomNodeType.PARAGRAPH)
+            this.selectionStateMachine
         );
+        this.backspaceHandler = new BackspaceHandler(this.sync);
+        this.enterHandler = new EnterHandler(this.sync);
+        this.command = new Command(
+            this.sync,
+            editorDiv,
+            this.compositionStateMachine,
+            this.selectionStateMachine,
+            this.backspaceHandler,
+            this.enterHandler
+        );
+        // EditorDebugger는 포함하지 않음
     }
 }
